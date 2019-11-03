7a4292d91ecec0345b9aac4384e30c23984356a5
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-03 18:55:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,9 @@
-# Your code here
-# For output purposes, use "puts" instead of "print" or "p"
+def meal_choice(veg1, veg2, protein = 'meat')
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meat = "tofu"
+veg1 = "broccoli"
+veg2 = "macaroni"
+meal_choice(veg1, veg2, meat)
